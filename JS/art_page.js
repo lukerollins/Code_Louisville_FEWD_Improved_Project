dc24aa02260bf5d0d55bbeea677dbca87bf6658d
Luke A Rollins <lukerllns@gmail.com>
2017-10-31 19:03:10 -0400

    Added comments to the JavaScript

--- JS/art_page.js
+++ JS/art_page.js
@@ -1,20 +1,22 @@
+//Opens the modal
 function openModal() {
     document.getElementById('myModal').style.display = "block";
          }
-           
+//Closes the modal           
 function closeModal() {
     document.getElementById('myModal').style.display = "none";
          }
-
+//When you click a particular thumbnail that particular slide it represents will open
 var slideIndex = 1;
          showsSlides(slideIndex);
 function plusSlides(n) {
              showSlides(slideIndex += n);
          }
-        
+//So both you know what slide you are on        
 function currentSlide(n) {
              showSlides(slideIndex = n);
          }
+//How the whole lightbox gallery works. Kind of like a slideshow in a sense
 function showSlides(n) {
             var i;
             var slides = document.getElementsByClassName("mySlides");
