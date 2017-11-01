c192ffdc3aef279f7e6ea41cf1b9b12d2aa1545c
Luke A Rollins <lukerllns@gmail.com>
2017-11-01 17:30:45 -0400

    "Eliminated the excess code in the art_page.js file as well as some excess html code in the pages that contain art"

--- JS/art_page.js
+++ JS/art_page.js
@@ -12,7 +12,7 @@ var slideIndex = 1;
 function plusSlides(n) {
              showSlides(slideIndex += n);
          }
-//So both you know what slide you are on        
+//So you know what slide you are on        
 function currentSlide(n) {
              showSlides(slideIndex = n);
          }
@@ -20,17 +20,11 @@ function currentSlide(n) {
 function showSlides(n) {
             var i;
             var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementById("caption")
-            var captionText = document.getElementById("caption");
             if (n > slides.length) {slideIndex = 1}
             if (n < 1) {slideIndex = slides.length}
             for (i = 0; i < slides.length; i++) {
                 slides[i].style.display = "none";
             }
-            for (i = 0; i < dots.length; i++) { dots[i].className = dots[i].className.replace("active", "");
-            }
             slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += "active";
-            captionText.innerHTML = dots[slideIndex-1].alt;
             }
             
