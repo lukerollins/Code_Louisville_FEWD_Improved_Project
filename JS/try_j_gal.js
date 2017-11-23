eaef162a7bd7547a41483596ddb35b49b70bcb8e
Luke A Rollins <lukerllns@gmail.com>
2017-11-23 01:09:01 -0500

    Finally starting to get the hang of using jGallery. Hoping to use it, but I dunno

--- JS/try_j_gal.js
+++ JS/try_j_gal.js
@@ -2,46 +2,26 @@ $( function() {
     $( '#gallery' ).jGallery({
         items: [
             {
-                title: 'Album 1',
+                title: 'Recycled book sketchbook',
                 images: [
                     {
-                        url: 'Imgs/front%20view%20recycled%20sketchbook%20portfolio.png',
+                        url: 'Imgs/front view recycled sketchbook portfolio.png',
                         thumbUrl: 'Imgs/sketchbook recyleced front thumb.jpg',
-                        title: 'Photo1'
+                        title: 'Exterior view of recycled book sketchbook.'
                     },
                     {
-                        url: 'Imgs/interior%20view%20recycled%20sketchbook%20porfolio.png',
+                        url: 'Imgs/interior view recycled sketchbook porfolio.png',
                         thumbUrl: 'Imgs/sketchbook recycled interior thumb.jpg',
-                        title: 'Photo2'
+                        title: 'Interior view of recycled book sketchbook.'
                     },
                     {
-                        url: 'Imgs/side%20view%20recycled%20sketchbook%20portfolio.png',
+                        url: 'Imgs/side view recycled sketchbook portfolio.png',
                         thumbUrl: 'Imgs/sketchbook recycled book side view thumb.jpg',
-                        title: 'Photo3'
+                        title: 'Side view of recycled book sketchbook.'
                     }
                 ]
             },
             
-            {
-                title: 'Album 2',
-                images: [
-                    {
-                        url: 'static/jgallery/images/large/4.jpg',
-                        thumbUrl: 'static/jgallery/images/thumbs/4.jpg',
-                        title: 'Photo4'
-                    },
-                    {
-                        url: 'static/jgallery/images/large/5.jpg',
-                        thumbUrl: 'static/jgallery/images/thumbs/5.jpg',
-                        title: 'Photo5'
-                    },
-                    {
-                        url: 'static/jgallery/images/large/6.jpg',
-                        thumbUrl: 'static/jgallery/images/thumbs/6.jpg',
-                        title: 'Photo6'
-                    }
-                ]
-            }
         ]
     });
 } );
