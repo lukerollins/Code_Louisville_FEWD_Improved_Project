e311d21a3b7a2b043366428c2508230f278d0951
Luke Rollins <lukerllns@gmail.com>
2019-06-25 23:07:34 -0400

    Got things looking good, with the exception of my index and contact pages. They still need work

--- JS/Masonary-called.js
+++
@@ -1,8 +0,0 @@
-//Masonry can be initialized with Nilla JS
-
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-    itemSelector: 'grid',
-    isFitWidth: true
-    
-});
